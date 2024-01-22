4f6c9962ff2f711540d280ba4bd7c638d7f7a785
joakiast <joakimstrand1998@gmail.com>
2024-01-22 15:03:00 +0100

    kjører trening dårlig output

--- vanilla_GAN.py
+++ vanilla_GAN.py
@@ -18,20 +18,20 @@ train_set_path = pathlib.Path("test")
 image_paths = [str(path) for path in list(train_set_path.glob('*.jpg'))]  # Bruk '*.png' eller annet hvis bildene dine har en annen filtype
 
 # Funksjon for å lese og forbehandle bildene
-resize_x = 160
-resize_y = 160
+resize_x = 64
+resize_y = 64
 def load_and_preprocess_image(path):
 
     image = tf.io.read_file(path)
     image = tf.image.decode_jpeg(image, channels=3)  # Bruk tf.image.decode_png for PNG-bilder, etc.
-    image = tf.image.resize(image, [resize_x, resize_y]) #ønsket resize størrelse, jo mindre jo raskere og dårligere kvalitet
+    image = tf.image.resize(image, [resize_y, resize_x],method=tf.image.ResizeMethod.AREA) #ønsket resize størrelse, jo mindre jo raskere og dårligere kvalitet
     image = tf.cast(image, tf.float32)
-    image = (image - 127.5) / 127.5  # Normaliser bildene til [-1, 1] området
+    image = (image / 127.5) -1  # Normaliser bildene til [-1, 1] området
     return image
 
 BUFFER_SIZE = len(image_paths)
 BATCH_SIZE = 100
-EPOCHS = 50
+EPOCHS = 100
 #print(BUFFER_SIZE)
 
 # Opprett en tf.data.Dataset
@@ -82,24 +82,24 @@ def make_generator_model(input_size_noise_x, input_size_noise_y):
     conv3_kernel_size = (5,5)
 
     model = tf.keras.Sequential()
-    model.add(layers.Dense(input_size_noise_x*input_size_noise_y*depth_feature_map, use_bias=False, input_shape=(noise_vector,)))
+    model.add(layers.Dense(input_size_noise_x*input_size_noise_y*depth_feature_map, use_bias=True, input_shape=(noise_vector,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((input_size_noise_x, input_size_noise_y, depth_feature_map)))
     assert model.output_shape == (None, input_size_noise_x, input_size_noise_y, depth_feature_map)  # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(conv1_filters, conv1_kernel_size, strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(conv1_filters, conv1_kernel_size, strides=(1, 1), padding='same', use_bias=True))
     assert model.output_shape == (None, input_size_noise_x, input_size_noise_y, conv1_filters)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(conv2_filters, conv2_kernel_size, strides=(2, 2), padding='same', use_bias=False)) #filter reduce stride = 2
+    model.add(layers.Conv2DTranspose(conv2_filters, conv2_kernel_size, strides=(2, 2), padding='same', use_bias=True)) #filter reduce stride = 2
     assert model.output_shape == (None, input_size_noise_x*2, input_size_noise_y*2, conv1_filters/2) #strides increase the size (14,14,64)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(conv3_filters, conv3_kernel_size, strides=(2, 2), padding='same', use_bias=False, activation='tanh')) # filter = 1 we want black white, rgb conv3 = 3
+    model.add(layers.Conv2DTranspose(conv3_filters, conv3_kernel_size, strides=(2, 2), padding='same', use_bias=True, activation='tanh')) # filter = 1 we want black white, rgb conv3 = 3
     assert model.output_shape == (None, input_size_noise_x*2*2, input_size_noise_y*2*2, conv3_filters) #a test that our image has the expected shape
 
     return model
@@ -225,7 +225,8 @@ def generate_and_save_images(model, epoch, test_input):
 
   plt.savefig(os.path.join(folder_name,'image_at_epoch_{:04d}.png'.format(epoch)))
   if epoch % 10 == 0:
-      plt.close(fig)
+      plt.savefig(os.path.join(folder_name, 'image_at_epoch_{:04d}.png'.format(epoch)))
+      #plt.close(fig)
   #plt.show()
   #plt.savefig(‘din_fig.png’)
 
