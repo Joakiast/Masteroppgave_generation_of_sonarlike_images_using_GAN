34535cbe5f1a83cbe660b11747b24bc8d865fb1a
joakiast <joakimstrand1998@gmail.com>
2024-02-15 14:44:24 +0100

    fikset opp i sirkel feil

--- pix2pix_inpainting.py
+++ pix2pix_inpainting.py
@@ -13,6 +13,7 @@ import time
 import pathlib
 from IPython import display
 import datetime
+import sys
 #import cv2
 #from sklearn.cluster import KMeans
 
@@ -159,11 +160,11 @@ def load_and_preprocess_image(path_image):
 
         #if not "clutter" in image_type:
         x,y = find_coordinates_for_circle(path_image)
-        inp_image = remove_part_of_image(image,x,y)#crop_image_around_POI(image, x, y, crop_size)
+        inp_image = remove_part_of_image(image,x,y) #crop_image_around_POI(image, x, y, crop_size)
         #image = tf.image.resize(image, [resize_x,resize_y], method=tf.image.ResizeMethod.AREA)
         #print(f"alle bilder kommer hit: image shape før resize: {image.shape} bilde: {path_image}")
         image = tf.image.resize(image, [resize_x, resize_y], method=tf.image.ResizeMethod.LANCZOS5)
-        inp_image = tf.image.resize(image, [resize_x, resize_y], method=tf.image.ResizeMethod.LANCZOS5)
+        inp_image = tf.image.resize(inp_image, [resize_x, resize_y], method=tf.image.ResizeMethod.LANCZOS5)
 
         return inp_image, image
         # else:
@@ -194,21 +195,33 @@ real_augmented_training_data_flip_up_down = []
 inp_augmented_training_data_rotate = []
 real_augmented_training_data_rotate = []
 
-
+i = 0
 for image_path in image_paths_train:
     inp, re = load_and_preprocess_image(image_path)
-    #
-    # # Konverterer tensor til uint8 ved å bruke tf.cast etter å ha skalert den
-    # inp_uint8 = tf.cast(inp * 255, tf.uint8)
-    #
-    # # Konverterer tensor til numpy-array for å kunne vises med matplotlib
-    # inp_numpy = inp_uint8.numpy()
-    #
-    # plt.figure()
-    # plt.title("inp")
-    # plt.imshow(inp_numpy)
-    # plt.show()  # Viser bildet
 
+    # Konverterer begge tensorer til uint8 ved å bruke tf.cast etter å ha skalert dem
+    inp_uint8 = tf.cast(inp * 255, tf.uint8)
+    re_uint8 = tf.cast(re * 255, tf.uint8)
+
+    # Konverterer tensorer til numpy-arrays for å kunne vises med matplotlib
+    inp_numpy = inp_uint8.numpy()
+    re_numpy = re_uint8.numpy()
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.title("inp")
+    plt.imshow(inp_numpy)
+
+    plt.subplot(1, 2, 2)
+    plt.title("re")
+    plt.imshow(re_numpy)
+
+    plt.show()
+
+    i += 1  # Øker telleren for hver iterasjon
+    if i > 9:
+        sys.exit()
     # tf.py_function(func=find_coordinates_for_cropping_tensor, inp=[path_image], Tout=[tf.float32,tf.float32])
     # plt.show()
     if "rock_RGB" in image_type or "oil_drum_RGB" in image_type or "man_made_object_RGB" in image_type:
