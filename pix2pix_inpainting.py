65a9787f8f0bc3f119641fd43053f59e12018a23
joakiast <joakimstrand1998@gmail.com>
2024-02-16 13:58:36 +0100

    endret kernel size fra 4 til 2

--- pix2pix_inpainting.py
+++ pix2pix_inpainting.py
@@ -360,24 +360,24 @@ def Generator():
   inputs = tf.keras.layers.Input(shape=[256, 256, 3])
 
   down_stack = [
-    downsample(128, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
-    downsample(256, 4),  # (batch_size, 64, 64, 128)
-    downsample(512, 4),  # (batch_size, 32, 32, 256)
-    downsample(1024, 4),  # (batch_size, 16, 16, 512)
-    downsample(1024, 4),  # (batch_size, 8, 8, 512)
-    downsample(1024, 4),  # (batch_size, 4, 4, 512)
-    downsample(1024, 4),  # (batch_size, 2, 2, 512)
-    downsample(1024, 4),  # (batch_size, 1, 1, 512)
+    downsample(128, 2, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
+    downsample(256, 2),  # (batch_size, 64, 64, 128)
+    downsample(512, 2),  # (batch_size, 32, 32, 256)
+    downsample(1024, 2),  # (batch_size, 16, 16, 512)
+    downsample(1024, 2),  # (batch_size, 8, 8, 512)
+    downsample(1024, 2),  # (batch_size, 4, 4, 512)
+    downsample(1024, 2),  # (batch_size, 2, 2, 512)
+    downsample(1024, 2),  # (batch_size, 1, 1, 512)
   ]
 
   up_stack = [
-    upsample(1024, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
-    upsample(1024, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
-    upsample(1024, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-    upsample(1024, 4),  # (batch_size, 16, 16, 1024)
-    upsample(512, 4),  # (batch_size, 32, 32, 512)
-    upsample(256, 4),  # (batch_size, 64, 64, 256)
-    upsample(128, 4),  # (batch_size, 128, 128, 128)
+    upsample(1024, 2, apply_dropout=True),  # (batch_size, 2, 2, 1024)
+    upsample(1024, 2, apply_dropout=True),  # (batch_size, 4, 4, 1024)
+    upsample(1024, 2, apply_dropout=True),  # (batch_size, 8, 8, 1024)
+    upsample(1024, 2),  # (batch_size, 16, 16, 1024)
+    upsample(512, 2),  # (batch_size, 32, 32, 512)
+    upsample(256, 2),  # (batch_size, 64, 64, 256)
+    upsample(128, 2),  # (batch_size, 128, 128, 128)
   ]
 
   initializer = tf.random_normal_initializer(0., 0.02)
@@ -435,16 +435,16 @@ def Discriminator():
     inp = tf.keras.layers.Input(shape=[256,256,3], name='input_image')
     tar = tf.keras.layers.Input(shape=[256,256,3], name='target_image')
     x = tf.keras.layers.concatenate([inp, tar])
-    down1 = downsample(64,4,False)(x) # fordi vi har en batch size på 128,128,64
-    down2 = downsample(128,4)(down1) #batch size 64,64,128
-    down3 = downsample(256,4)(down2) #batch size ,32,32,256
+    down1 = downsample(64,2,False)(x) # fordi vi har en batch size på 128,128,64
+    down2 = downsample(128,2)(down1) #batch size 64,64,128
+    down3 = downsample(256,2)(down2) #batch size ,32,32,256
 
     zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)
-    conv = tf.keras.layers.Conv2D(512,4,strides=1, kernel_initializer=initializer,use_bias=True)(zero_pad1) #batch size ,31,31,512
+    conv = tf.keras.layers.Conv2D(512,2,strides=1, kernel_initializer=initializer,use_bias=True)(zero_pad1) #batch size ,31,31,512
     batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
     leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
     zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu) #batchsize,33,33,512
-    last = tf.keras.layers.Conv2D(1,4,strides=1, kernel_initializer=initializer)(zero_pad2) #batch size 30,30,1
+    last = tf.keras.layers.Conv2D(1,2,strides=1, kernel_initializer=initializer)(zero_pad2) #batch size 30,30,1
     return tf.keras.Model(inputs=[inp,tar], outputs=[last])
 
 discriminator = Discriminator()
