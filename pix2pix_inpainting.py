c060563ad3a55e11727b70807ae75614895079cc
joakiast <joakimstrand1998@gmail.com>
2024-02-19 19:53:14 +0100

    prøver med små kernel i tidligere lagene og større kernels i de dypere lagene, jeg la også til et ekstra upsamnple lag

--- pix2pix_inpainting.py
+++ pix2pix_inpainting.py
@@ -362,10 +362,10 @@ def Generator():
   down_stack = [
     downsample(128, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
     downsample(256, 4),  # (batch_size, 64, 64, 128)
-    downsample(512, 15),  # (batch_size, 32, 32, 256)
-    downsample(1024, 15),  # (batch_size, 16, 16, 512)
-    downsample(1024, 15),  # (batch_size, 8, 8, 512)
-    downsample(1024, 15),  # (batch_size, 4, 4, 512)
+    downsample(512, 10),  # (batch_size, 32, 32, 256)
+    downsample(1024, 10),  # (batch_size, 16, 16, 512)
+    downsample(1024, 10),  # (batch_size, 8, 8, 512)
+    downsample(1024, 10),  # (batch_size, 4, 4, 512)
     downsample(1024, 15),  # (batch_size, 2, 2, 512)
     downsample(1024, 15),  # (batch_size, 1, 1, 512)
   ]
@@ -373,10 +373,10 @@ def Generator():
   up_stack = [
     upsample(1024, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
     upsample(1024, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
-    upsample(1024, 15, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-    upsample(1024, 15, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-    upsample(1024, 15, apply_dropout=True),  # (batch_size, 16, 16, 1024)
-    upsample(512, 15),  # (batch_size, 32, 32, 512)
+    upsample(1024, 10, apply_dropout=True),  # (batch_size, 8, 8, 1024)
+    upsample(1024, 10, apply_dropout=True),  # (batch_size, 8, 8, 1024)
+    upsample(1024, 10, apply_dropout=True),  # (batch_size, 16, 16, 1024)
+    upsample(512, 10),  # (batch_size, 32, 32, 512)
     upsample(256, 15),  # (batch_size, 64, 64, 256)
     upsample(128, 15),  # (batch_size, 128, 128, 128)
   ]
@@ -437,7 +437,7 @@ def Discriminator():
     tar = tf.keras.layers.Input(shape=[256,256,3], name='target_image')
     x = tf.keras.layers.concatenate([inp, tar])
     down1 = downsample(128,4,False)(x) # fordi vi har en batch size på 128,128,64
-    down2 = downsample(256,15)(down1) #batch size 64,64,128
+    down2 = downsample(256,10)(down1) #batch size 64,64,128
     down3 = downsample(512,15)(down2) #batch size ,32,32,256
 
     zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)
