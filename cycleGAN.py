1d55d750ea6e86d72c64b28165ed50171d324795
joakiast <joakimstrand1998@gmail.com>
2024-03-02 13:14:02 +0100

    prøver mer resnet

--- cycleGAN.py
+++ cycleGAN.py
@@ -39,7 +39,7 @@ from numpy import iscomplexobj
 from scipy.linalg import sqrtm
 
 run = neptune.init_run(
-    project="masteroppgave/testRun",
+    project="masteroppgave/cycleGAN",
     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjMDY3ZDFlNS1hMGVhLTQ1N2YtODg4MC1hNThiOTM1NGM3YTQifQ=="
 )
 
