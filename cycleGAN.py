f7f57cd36677ef8745e73cd60c691edaf2abd84a
joakiast <joakimstrand1998@gmail.com>
2024-03-10 12:38:59 +0100

    lagt inn bege datasettene for det simulerte datasettet, fjernet dårlig data fra V1

--- cycleGAN.py
+++ cycleGAN.py
@@ -53,7 +53,7 @@ np.random.seed(seed_number)
 random.seed(seed_number)
 
 run = neptune.init_run(
-    project="masteroppgave/testRun",
+    project="masteroppgave/cycleGAN",
     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjMDY3ZDFlNS1hMGVhLTQ1N2YtODg4MC1hNThiOTM1NGM3YTQifQ=="
 )
 
