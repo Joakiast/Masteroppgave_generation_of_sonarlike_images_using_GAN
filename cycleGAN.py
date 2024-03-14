bf2db3e29d98bcf17d4c0ac0fedc09e22de4ace4
joakiast <joakimstrand1998@gmail.com>
2024-03-14 11:08:46 +0100

    lagt inn spectral normalization for discriminatoren

--- cycleGAN.py
+++ cycleGAN.py
@@ -106,8 +106,8 @@ beta_D_y = 0.9
 
 save_every_n_epochs = 2
 
-generator_type = "resnet" # virker som resnet gir best resultat
-#generator_type = "unet"
+#generator_type = "resnet" # virker som resnet gir best resultat
+generator_type = "unet"
 
 filter_muultiplier_generator = 2
 filter_muultiplier_discriminator = 1
